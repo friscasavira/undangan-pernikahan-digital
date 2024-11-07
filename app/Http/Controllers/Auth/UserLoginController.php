3933d1f3e81a67a6f4aba253324a6a5c25a2101e
antilarasati <antilarasati06@gmail.com>
2024-11-07 11:12:02 +0700

    membuat controller

---
+++ app/Http/Controllers/Auth/UserLoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserLoginController extends Controller
+{
+    public function login()
+    {
+        return view('auth.user_login');
+    }
+
+    public function auth(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (Auth::guard('user')->attempt($credentials)) {
+            return redirect()->route('user.dashboard');
+        }
+
+        return back()->withErrors(['login_error' => 'Username atau Password Salah.']) ->onlyInput('username');
+    }
+}
