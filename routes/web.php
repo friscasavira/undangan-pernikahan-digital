f2ccdf35c765f13635b77a7f92737461b544e780
antilarasati <antilarasati06@gmail.com>
2024-11-11 10:06:52 +0700

    hapus route

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['guest'])->group(function(){
-    Route::get('/admin/login', [UserLoginController::class, 'login'])->name('admin_login');
-    Route::post('/admin/submit', [UserLoginController::class, 'auth'])->name('admin.submit');
-
-});
-
-Route::middleware(['admin'])->group(function() {
-    Route::get('/admin/dashboard', [SettingController::class, 'dashboard'])->name('admin.dashboard');
-});
-
