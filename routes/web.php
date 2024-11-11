9a3d9542e85077feac26c6adebb81d4854941766
rillcuyy <anjimid86@gmail.com>
2024-11-11 10:08:36 +0700

    membuat halaman admin dan user

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::middleware(['guest:admin', 'guest:user'])->group(function(){
-    Route::get('/admin/login', [UserLoginController::class, 'login'])->name('admin.login');
-    Route::post('/admin/submit', [UserLoginController::class, 'submit'])->name('admin.submit');
-
-    Route::get('/user/login', [UserLoginController::class, 'login'])->name('user.login');
-    Route::post('/user/submit', [UserLoginController::class, 'submit'])->name('user.submit');
-
-});
-
-Route::middleware(['role:admin'])->group(function() {
-    Route::get('/admin/dashboard', [SettingsController::class, 'dashboard'])->name('admin.dashboard');
-});
-Route::middleware(['role:user'])->group(function() {
-    Route::get('/user/dashboard', [SettingsController::class, 'dashboard'])->name('user.dashboard');
-});
-
