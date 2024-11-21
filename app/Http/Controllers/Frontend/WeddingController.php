1cf3e56025ac51a34807b4b23f39760381ad68e1
antilarasati <antilarasati06@gmail.com>
2024-11-21 12:12:53 +0700

    menyesuaikan foto frontend

--- app/Http/Controllers/Frontend/WeddingController.php
+++ app/Http/Controllers/Frontend/WeddingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Models\events;
 use App\Models\love_story;
+use App\Models\photos;
 use App\Models\weddings;
 use Illuminate\Http\Request;
 
@@ -12,8 +13,10 @@ class WeddingController
     public function home()
     {
         $wedding = weddings::all()->first();
+        $id_wedding = $wedding->id_wedding;
         $love_storys = love_story::all();
         $events = events::all();
-        return view('frontend.home', compact('wedding', 'love_storys', 'events'));
+        $photos = photos::where('id_wedding', $id_wedding)->take(3)->get();
+        return view('frontend.home', compact('wedding', 'love_storys', 'events', 'photos'));
     }
 }
