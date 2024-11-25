0f6938a7bec89b6c530561eb1f9d6e0cde4e2ce2
antilarasati <antilarasati06@gmail.com>
2024-11-25 10:43:06 +0700

    frontend

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -506,137 +506,102 @@
 
     <!-- RSVP Form Start -->
     <div class="container-fluid RSVP-form py-5" id="weddingRsvp">
-        <div class="container py-5">
-            <div class="mb-5 text-center mx-auto wow fadeIn" data-wow-delay="0.1s" style="max-width: 800px;">
-                <h1 class="display-2 text-primary">RSVP Form</h1>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-10">
-                    <div class="p-5 border-secondary position-relative" style="border-style: double;">
-                        <div class="fw-bold text-primary bg-white d-flex align-items-center justify-content-center position-absolute border-secondary p-2 wow fadeIn" data-wow-delay="0.1s" style="width: 75%; border-style: double; top: 0; left: 50%; transform: translate(-50%, -50%);">
-                            Kindly respond by June 25, 2024 we look forward to celebrating with you!
-                        </div>
-                        <form>
-                            <div class="row gx-4 gy-3">
-                                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                                    <div class="form-group">
-                                        <label for="Examplename" class="form-label text-dark">Name</label>
-                                        <input type="text" class="form-control py-3 border-0" id="Examplename" placeholder="Name">
-                                        </div>
-                                </div>
-                                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                                    <div class="form-group">
-                                        <label for="Examplename" class="form-label text-dark">Email</label>
-                                        <input type="text" class="form-control py-3 border-0" id="Examplename" placeholder="Email">
-                                        </div>
-                                </div>
-                                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                                    <div class="form-group">
-                                        <label for="Examplename" class="form-label text-dark">Phone</label>
-                                        <input type="text" class="form-control py-3 border-0" id="Examplename" placeholder="Phone">
-                                        </div>
+    <div class="container py-5">
+        <div class="mb-5 text-center mx-auto wow fadeIn" data-wow-delay="0.1s" style="max-width: 800px;">
+            <h1 class="display-2 text-primary">RSVP Form</h1>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="p-5 border-secondary position-relative" style="border-style: double;">
+                    <div class="fw-bold text-primary bg-white d-flex align-items-center justify-content-center position-absolute border-secondary p-2 wow fadeIn" data-wow-delay="0.1s" style="width: 75%; border-style: double; top: 0; left: 50%; transform: translate(-50%, -50%);">
+                        Kindly respond by June 25, 2024, we look forward to celebrating with you!
+                    </div>
+                    <form method="POST" action="">
+                        <div class="row gx-4 gy-3">
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="form-group">
+                                    <label for="name" class="form-label text-dark">Name</label>
+                                    <input type="text" name="name" class="form-control py-3 border-0" id="name" placeholder="Name" required>
                                 </div>
                             </div>
-                                <div class="col-12 wow fadeIn" data-wow-delay="0.1s">
-                                    <div class="form-group">
-                                        <label for="exampletextarea" class="form-label text-dark">Message</label>
-                                        <textarea name="text" class="form-control border-0" id="exampletextarea" cols="30" rows="5" placeholder="Message"></textarea>
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="form-group">
+                                    <label for="email" class="form-label text-dark">Email</label>
+                                    <input type="email" name="email" class="form-control py-3 border-0" id="email" placeholder="Email" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="form-group">
+                                    <label for="phone" class="form-label text-dark">Phone</label>
+                                    <input type="text" name="phone" class="form-control py-3 border-0" id="phone" placeholder="Phone" required>
+                                </div>
+                            </div>
+                            <div class="col-12 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="form-group">
+                                    <label for="message" class="form-label text-dark">Message</label>
+                                    <textarea name="message" class="form-control border-0" id="message" cols="30" rows="5" placeholder="Message"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="form-group">
+                                    <label for="is_invited" class="form-label text-dark">Is Invited</label>
+                                    <input type="text" name="is_invited" class="form-control py-3 border-0" id="is_invited" placeholder="Yes/No" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="form-group">
+                                    <label for="attendance_status" class="form-label text-dark">Attendance Status</label>
+                                    <input type="text" name="attendance_status" class="form-control py-3 border-0" id="attendance_status" placeholder="Yes/No" required>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="form-group">
+                                    <label for="total_guest" class="form-label text-dark">Total Guests</label>
+                                    <input type="number" name="total_guest" class="form-control py-3 border-0" id="total_guest" placeholder="Total Guests" required>
+                                </div>
+                            </div>
+                            <div class="col-12 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="text-center border border-secondary p-4 my-4 position-relative">
+                                    <div class="fw-bold text-primary bg-white d-flex align-items-center justify-content-center position-absolute border-secondary p-2" style="width: 50%; border-style: double; top: 0; left: 50%; transform: translate(-50%, -50%);">
+                                        Will you be attending?
                                     </div>
-                                </div>
-                                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                                    <div class="form-group">
-                                        <label for="Examplename" class="form-label text-dark">is_invited</label>
-                                        <input type="text" class="form-control py-3 border-0" id="Examplename" placeholder="is_invited">
-                                        </div>
-                                </div>
-                                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                                    <div class="form-group">
-                                        <label for="Examplename" class="form-label text-dark">attendance_status</label>
-                                        <input type="text" class="form-control py-3 border-0" id="Examplename" placeholder="attendance_status">
-                                        </div>
-                                </div>
-                                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                                    <div class="form-group">
-                                        <label for="Examplename" class="form-label text-dark">total_guest</label>
-                                        <input type="text" class="form-control py-3 border-0" id="Examplename" placeholder="total_guest">
-                                        </div>
-                                </div>
-                                
-                                
-                                <div class="col-12 wow fadeIn" data-wow-delay="0.1s">
-                                    <div class="text-center border border-secondary p-4 my-4 position-relative">
-                                        <div class="fw-bold text-primary bg-white d-flex align-items-center justify-content-center position-absolute border-secondary p-2" style="width: 50%; border-style: double; top: 0; left: 50%; transform: translate(-50%, -50%);">
-                                            Will you be attending?
-                                        </div>
-                                        <div class="mt-4">
-                                            <div class="row g-4">
-                                                <div class="col-lg-6">
-                                                    <div class="d-flex justify-content-center">
-                                                        <div class="form-check d-flex">
-                                                            <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                                                            <label class="form-check-label text-dark" for="flexRadioDefault1">
-                                                                Accepts With Pleasure!
-                                                            </label>
-                                                        </div>
+                                    <div class="mt-4">
+                                        <div class="row g-4">
+                                            <div class="col-lg-6">
+                                                <div class="d-flex justify-content-center">
+                                                    <div class="form-check d-flex">
+                                                        <input class="form-check-input me-2" type="radio" name="attendance_option" value="Accepts With Pleasure" id="accept" required>
+                                                        <label class="form-check-label text-dark" for="accept">
+                                                            Accepts With Pleasure!
+                                                        </label>
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-6">
-                                                    <div class="d-flex justify-content-center">
-                                                        <div class="form-check d-flex">
-                                                            <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                                                            <label class="form-check-label text-dark" for="flexRadioDefault1">
-                                                                Declines With Regret
-                                                            </label>
-                                                        </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="d-flex justify-content-center">
+                                                    <div class="form-check d-flex">
+                                                        <input class="form-check-input me-2" type="radio" name="attendance_option" value="Declines With Regret" id="decline">
+                                                        <label class="form-check-label text-dark" for="decline">
+                                                            Declines With Regret
+                                                        </label>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-12 text-center wow fadeIn" data-wow-delay="0.1s">
-                                    <button class="btn btn-primary btn-primary-outline-0 py-3 px-5">Submit Now</button>
-                                </div>
                             </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="mt-5">
-                <div class="row g-5 justify-content-center text-center">
-                    <div class="col-md-4 wow fadeIn" data-wow-delay="0.1s">
-                        <div class="p-4 border-secondary" style="border-style: double;">
-                            <h4>Call Us Now</h4>
-                            <a href="#" class="text-dark">+123 456 7890</a>
+                            <div class="col-12 text-center wow fadeIn" data-wow-delay="0.1s">
+                                <button class="btn btn-primary btn-primary-outline-0 py-3 px-5" type="submit">Submit Now</button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-4 wow fadeIn" data-wow-delay="0.3s">
-                        <div class="p-4 border-secondary" style="border-style: double;">
-                            <h4>When</h4>
-                            <p class="mb-0 text-dark">
-                                28. June 2024. at 7 o’clock in the evening
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-4 wow fadeIn" data-wow-delay="0.5s">
-                        <div class="p-4 border-secondary" style="border-style: double;">
-                            <h4>Where</h4>
-                            <p class="mb-0 text-dark">
-                                Niloy Hotel, 123 ranking Street, New York.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-12 wow fadeIn" data-wow-delay="0.2s">
-                        <div class="border-secondary" style="border-style: double;">
-                            <iframe class="w-100"
-                                style="height: 450px; margin-bottom: -6px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.33750346623!2d-73.97968099999999!3d40.6974881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1694259649153!5m2!1sen!2sbd"
-                                loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
     <!-- RSVP Form End -->
 
 
