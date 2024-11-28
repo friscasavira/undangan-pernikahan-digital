ea0b36cd24875ef522644516f1d95fe33d217415
iqblhnf <iqbalhanif30052004@gmail.com>
2024-11-28 09:02:03 +0700

    perbaikan profile user

--- resources/views/backend/user/profile.blade.php
+++ resources/views/backend/user/profile.blade.php
@@ -12,42 +12,43 @@
                 {{ session('success') }}
             </div>
             @endif
-            <div class="d-flex align-items-center justify-content-center ms-4 mb-4">
-                <div class="position-relative">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{ route('user.profile.update') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="email" class="form-label">email</label>
+                            <input type="text" class="form-control" id="email" name="email" value="{{ old('email', $profile->email) }}">
+                            <div class="text-danger">
+                                @error('email')
+                                {{ $message }}
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="username" class="form-label">User Name</label>
+                            <input type="username" class="form-control" id="username" name="username" value="{{ old('username', $profile->username) }}">
+                            <div class="text-danger">
+                                @error('username')
+                                {{ $message }}
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" value="{{ old('password', $profile->password) }}">
+                                <div class="text-danger">
+                                    @error('password')
+                                    {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            </div>
+                    </form>
+                </div>
             </div>
-            <form action="{{ route('user.profile.update') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                <div class="mb-3">
-                    <label for="email" class="form-label">email</label>
-                    <input type="text" class="form-control" id="email" name="email" value="{{ old('email', $profile->email) }}">
-                    <div class="text-danger">
-                        @error('email')
-                        {{ $message }}
-                        @enderror
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label for="username" class="form-label">User Name</label>
-                    <input type="username" class="form-control" id="username" name="username" value="{{ old('username', $profile->username) }}">
-                    <div class="text-danger">
-                        @error('username')
-                        {{ $message }}
-                        @enderror
-                    </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" value="{{ old('password', $profile->password) }}">
-                    <div class="text-danger">
-                        @error('password')
-                        {{ $message }}
-                        @enderror
-                    </div>
-                </div>
-                <div class="text-center">
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                </div>
-            </form>
         </div>
     </div>
 </div>
